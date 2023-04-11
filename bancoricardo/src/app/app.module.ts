e7abf674db53da5b12b262c46dbc559538f27d9f
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2023-04-10 22:38:32 -0300

    página home e clientes

--- bancoricardo/src/app/app.module.ts
+++ bancoricardo/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
+    HomeComponent,
+    ClientesComponent,
   ],
   imports: [
     BrowserModule,
