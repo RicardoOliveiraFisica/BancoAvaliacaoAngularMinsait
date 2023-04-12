2e25dca85a4539c49e3d465f454895d1ec345be1
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2023-04-12 15:33:50 -0300

    modulo para cadastro e atualizacao

---
+++ bancoricardo/src/app/pages/register-update-clients/register-update-clients.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-update-clients',
+  templateUrl: './register-update-clients.component.html',
+  styleUrls: ['./register-update-clients.component.css']
+})
+export class RegisterUpdateClientsComponent {
+
+}
