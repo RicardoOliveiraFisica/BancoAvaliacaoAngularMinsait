eafeb899c51e486c6c342a305b63fa063771100c
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2023-04-13 23:11:21 -0300

    ajustes de layout

--- bancoricardo/src/app/components/button/button.component.ts
+++ bancoricardo/src/app/components/button/button.component.ts
@@ -7,5 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class ButtonComponent {
   @Input() label: string = '';
-  @Input() color: string = 'dark';
+  @Input() bGcolor: string = 'dark';
+  @Input() color: string = 'white';
+
 }
